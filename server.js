52f26339c8e3a5bc568c121897daaa3031eb1373
Thales Spanhol <thales_spanhol@hotmail.com>
2024-06-25 22:28:01 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+import { createServer } from 'node:http'
+
+const server = createServer((request, response) => {
+    response.write('Hello World');
+    return response.end()
+})
+
+server.listen(3333)
